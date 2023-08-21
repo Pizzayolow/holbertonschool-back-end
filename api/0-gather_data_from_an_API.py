881490af598ce23147ad03079194461f778bdd9a
Pizzayolow <ralliard.valentin@gmail.com>
2023-08-21 11:53:58 +0200

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,33 +1,52 @@
 #!/usr/bin/python3
-"""Requests some data with API and return infos"""
+""" Using the REST API jsonplaceholder """
 import requests
 import sys
 
-# Requests some data with API
 if __name__ == "__main__":
-    # Get the name of the employee by ID
-    employee_id = {'id': sys.argv[1]}
+    if len(sys.argv) < 2:
+        print("Usage: script_name employee_id")
+        sys.exit(1)
+
+    employee_id = {"id": sys.argv[1]}
+
+    # Get the name of the employee
     response = requests.get(
-        'https://jsonplaceholder.typicode.com/users/', params=employee_id)
-    data = response.json()
-    dict_d = data[0]  # Create a dictionary to extract the name
+        "https://jsonplaceholder.typicode.com/users/", params=employee_id
+    )
+    if response.status_code != 200:
+        print("Error fetching employee data")
+        sys.exit(1)
 
-    # Get the tasks
-    user_id = {'userId': sys.argv[1]}
-    tasks = requests.get(
-        'https://jsonplaceholder.typicode.com/todos', params=user_id)
-    dt_tk = tasks.json()
+    employee_data = response.json()
+    name_employee = employee_data[0]["name"]
 
-    # Create a counter to know the number of completed tasks
-    ct = 0
-    list_task_completed = []
-    for element in dt_tk:
-        if element['completed'] is True:
-            list_task_completed.append((element["title"]))
-            ct += 1
+    # Get the total number of tasks
+    user_id = {"userId": sys.argv[1]}
+    task_response = requests.get(
+        "https://jsonplaceholder.typicode.com/todos", params=user_id
+    )
+    if task_response.status_code != 200:
+        print("Error fetching task data")
+        sys.exit(1)
 
-    # Print final output and format the prompt
+    tasks_data = task_response.json()
+    number_total_task = len(tasks_data)
+
+    # Get the number of tasks completed
+    tasks_completed = 0
+    tasks_title = []
+
+    for task in tasks_data:
+        if task["completed"]:
+            tasks_completed += 1
+            tasks_title.append(task["title"])
+
+    # print final result
     print(
-        f"Employee {dict_d['name']} is done with tasks({ct}/{len(dt_tk)}):")
-    for element in list_task_completed:
-        print("\t" + ' ' + element)
+        f"Employee {name_employee} is done with tasks"
+        f"({tasks_completed}/{number_total_task}):"
+    )
+
+    for title in tasks_title:
+        print("\t" + title)
